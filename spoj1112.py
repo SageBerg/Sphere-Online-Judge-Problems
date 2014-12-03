92a5295b1bd573d826e2af332c0d51b8b30ea34e
Sage Berg <sberg@uoregon.edu>
2014-12-02 21:41:48 -0800

    solved problem 1112: Number Steps (predicts values at Cartesian coords based on snakey number pattern)

---
+++ spoj1112.py
@@ -0,0 +1,22 @@
+"""
+SPOJ 1112: Number Steps
+Sage Berg
+2 Dec 2014
+"""
+
+def num_at_coords(x, y):
+    answer = "No Number"
+    if x == y:
+        answer = 4 * (x // 2)
+        if x % 2 != 0:
+            answer += 1
+    elif x == y + 2:
+        answer = 4 * (x // 2) - 2
+        if x % 2 != 0:
+            answer += 1
+    print(answer)
+
+n = int(input())
+for i in range(n):
+    x, y = input().split()
+    num_at_coords(int(x), int(y))
