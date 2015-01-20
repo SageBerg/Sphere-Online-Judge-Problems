39e8c981de70343b1d1d404a643fdf728a3ea353
Sage Berg <sberg@uoregon.edu>
2015-01-19 21:27:36 -0800

    solved it

--- rectangles.py
+++ rectangles.py
@@ -1,5 +1,10 @@
 def main():
     squares = int(input())
-    
+    i = 1
+    recs = 0
+    while i * i <= squares:
+        recs += ((squares // i) - (i - 1))
+        i += 1
     print(recs)
+
 main()
