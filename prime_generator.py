1bed7727bc3fff5bcb61e2cc7808f1d8377506ab
Sage Berg <sberg@uoregon.edu>
2015-11-16 22:03:31 -0800

    cleaned up a bit, still need to do bounded sieve

--- prime_generator.py
+++ prime_generator.py
@@ -1,15 +1,9 @@
-import time
-import math
-
-def is_prime(n):
-    for i in range(2, int(math.sqrt(n))):
-        if float(n)/i == n/i:
-            return False
-    return True
+from time import time
+from math import sqrt
 
 def sieve_of_eratosthenes(n):
     primes = [True] * n
-    for i in range(2, int(math.sqrt(n))):
+    for i in range(2, int(sqrt(n))):
         if primes[i]:
             j = i**2
             while j < n:
@@ -17,15 +11,17 @@ def sieve_of_eratosthenes(n):
                 j += i
 
 def print_primes(start, end):
-    begin = time.time() 
-    for i in range(start, end):
-        is_prime(i)
-    print "time taken: " + str(time.time() - begin)
+    begin = time() 
+    for i in range(start, end + 1):
+        pass
+    print "\ntime taken: " + str(time() - begin)
 
 cases = int(raw_input())
 
-for _ in range(cases):
+for i in range(cases):
     case = raw_input().split()
     start = int(case[0])
     end   = int(case[1])
     print_primes(start, end)
+    if i != cases -1:
+        print
